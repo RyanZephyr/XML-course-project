2a03d82ce39e945ab56f4666a996102c639eefd3
Ryan <cn_ryan@qq.com>
2019-05-19 09:42:56 +0800

    commit

---
+++ movie-bot/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // You can add your own open weather map token here (https://home.openweathermap.org/api_keys)
+  // OPENWEATHER_TOKEN: process.env.OPENWEATHER_TOKEN || '',
+  // You can add your own tmdb token here (https://www.themoviedb.org/)
+  MOVIEDB_TOKEN: process.env.MOVIEDB_TOKEN || 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NzI4YTE1MTEzNWYwYjRjY2VjZGI1MWZlMGM3ZjIzMSIsInN1YiI6IjVjZTBiMDc3YzNhMzY4MjNjNTFmZmRkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ERLU1rVtTVOU2WHMUBhbyFPEv75s0XppIWUlLqqEtMk',
+  PORT: process.env.PORT || 5000,
+};
